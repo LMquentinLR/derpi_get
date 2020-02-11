76de2b8db06b40b84b394a45d293a9ebe414e4c9
LMquentinLR <quentin.leroux@edhec.com>
2020-02-10 19:21:16 -0500

    review feb20 v0.03

--- derpi_get/core_class.py
+++ derpi_get/core_class.py
@@ -12,28 +12,29 @@ def error_message(error_type, location):
     """
     Creates a custom error message.
     ---
-    :param <error_type>: <str> ; type of occuring error
-    :param <location>: <str> ; method name where the error occured
+    :param <error_type>: <str> ; short description of occuring error
+    :param <location>: <str> ; name of method where the error occured
     """
     return f"An {error_type} error was raised in the {location} method of the class img_metadata"+\
         "in the derpi_get/core_class.py file."
 
 class img_metadata:
     """
-    Class object that describes how to retrieve picture metadata from derpibooru's
+    Object implementing how to retrieve picture metadata from derpibooru's
     REST API. Data is retrieved as a series of c. 1Mb JSON files.
     """
     def __init__(self, tags = [], at_least_one = True, instances = 10):
         """
-        Initializes of the img_metadata class object.
+        Initializes the img_metadata class object.
         ---
-        :param <tags>: <list> ; list of strings (i.e. picture tags)
-        :param <at_least_one>: <boolean> ; toggles 'at least one tag' option instead of 'all tags'
-        :param <instances>: <integer> ; number of instances/loops allowed before stop
+        :param <tags>: <list> ; list picture tags used for sorting and extracting metadata
+        :param <at_least_one>: <boolean> ; toggles whether or not data to be retrieved must satisfy at least one tag or all of them
+        :param <instances>: <integer> ; number of derpibooru image requests allowed before stopping
         """      
-        assert(not isinstance(tags, list)), error_message("Erroneous Type", "__init__")
-        assert(not isinstance(instances, int)), error_message("Erroneous Type", "__init__")
-        assert(not isinstance(at_least_one, bool), error_message("Erroneous Type", "__init__")
+        assert(isinstance(tags, list)), error_message("Erroneous Type", "__init__")
+        assert((isinstance(instances, int)) or instances == ""), error_message("Erroneous Type", "__init__")
+        assert(isinstance(at_least_one, bool)), error_message("Erroneous Type", "__init__")
+        
         self.tags = tags
         self.at_least_one = at_least_one
         self.instances = instances
@@ -44,15 +45,15 @@ class img_metadata:
         ---
         :param <bytes_size>: <integer> ; size in bytes of a file
         """
-        assert(not isinstance(bytes_size, float)), error_message("Erroneous Type", "bytes_length")
-        for unit_multiple in ['bytes', 'KB', 'MB', 'GB', 'TB']:
-            if bytes_size < 1024.0: return "%3.1f %s" % (bytes_size, unit_multiple)
+        assert(isinstance(bytes_size, float)), error_message("Erroneous Type", "bytes_length")
+        for unit_multiple in ["bytes", "KB", "MB", "GB", "TB"]:
+            if bytes_size < 1024.0: return f"{round(bytes_size, 2)} {unit_multiple}."
             bytes_size /= 1024.0
     
     def keys_to_keep(self):
         """
-        Returns the keys to keep in the extracted JSON data.
-        keys available:
+        Returns the keys to be kept in the extracted JSON data.
+        List of keys available:
         {"id", "created_at", "updated_at", "first_seen_at", "tags", "tag_ids", "uploader_id",
         "score", "comment_count", "width", "height", "tag_count", "file_name", "description",
         "uploader", "image", "upvotes", "downvotes", "faves", "aspect_ratio", "original_format",
@@ -66,21 +67,21 @@ class img_metadata:
     
     def json_split_size(self, path):
         """
-        Splits a json file if it is too large (1Mb).
+        Splits a json file if it is too large (>1Mb).
         ---
         :param <path>: <string> ; path of a json file to weigh
         """
-        assert(not isinstance(path, str)), error_message("Erroneous Type", "json_split_size")
-        max_size_reached_message = "JSON MAX FILE SIZE REACHED: JSON file to be archived and a new one created."
+        assert(isinstance(path, str)), error_message("Erroneous Type", "json_split_size")
+        max_size_reached_message = "MAX JSON FILE SIZE REACHED: file to be split."
 
         length = self.bytes_length(float(os.stat(path).st_size))
         length = length.split(" ")
 
         if float(length[0]) >= 1.0 and length[1] == "MB":
-            nb_file = -1
+            nb_file = 0
             while True:
-                nb_file += 1
                 new_path = path[:-5] + "_" + str(nb_file) + ".json"
+                nb_file += 1
                 if os.path.exists(new_path) == False:
                     print(max_size_reached_message)
                     try:
@@ -98,31 +99,32 @@ class img_metadata:
         Checks if prior JSON extractions exist in the working directory (possibly created by self.json_split_size()
         The default file name is 'derpibooru_metadata.json'.
         ---
-        :param <print_msg>: <boolean> ; toggle between 'prints message to command line' and 'prints nothing'
+        :param <print_msg>: <boolean> ; toggles between printing messages to the cmd or printing nothing
         """
-        def create_messages(item):
-            """
-            Creates a dictionary of warning messages.
-            ---
-            :param <item>: <str> ; name of item.
-            """
-            return {"created":f"{item} created in {os.getcwd()}.",
-            "not_created":f"{item} not created in {os.getcwd()}.",
-            "found":f"{item} found in {os.getcwd()}.",
-            "not_found":f"{item} not found in {os.getcwd()}."}
-
-        assert(not isinstance(print_msg, bool)), error_message("Erroneous Type", "check_prior_extract")        
         
-        folder_messages = create_messages("Folder ./data")
-        json_messages = create_messages("File derpibooru_metadata.json")
+        def create_warnings(file):
+            """
+            Creates a dictionary of warning messages related to file management.
+            ---
+            :param <file>: <str> ; name of item.
+            """
+            return {"created":f"{file} created in {os.getcwd()}.",
+            "not_created":f"{file} not created in {os.getcwd()}.",
+            "found":f"{file} found in {os.getcwd()}.",
+            "not_found":f"{file} not found in {os.getcwd()}."}
+
+        assert(isinstance(print_msg, bool)), error_message("Erroneous Type", "check_prior_extract")        
+        
+        folder_messages = create_warnings("Folder ./data")
+        json_messages = create_warnings("File derpibooru_metadata.json")
         json_path = os.getcwd() + "\\data\\derpibooru_metadata.json"
         
-        # Checks if json exists
+        # Checks if a json file exists
         if os.path.exists(json_path):
             if print_msg: print(json_messages["found"])
         else:
-            # If json doesn't exist, checks if folder ./data exists
-            # If folder doesn't exist, creates it
+            # If the json file doesn't exist, checks if the folder ./data exists
+            # If the folder doesn't exist, creates it
             try:
                 if print_msg: print(json_messages["not_found"])
                 if not os.path.exists(os.getcwd() + "\\data"):
@@ -141,17 +143,18 @@ class img_metadata:
         """
         Retrieves picture metadata from the derpibooru REST API.
         """
+        
         def back_off(counter, error):
             """
             In case of a request error, creates a back-off as requested by the derpibooru ToS.
             ---
-            :param <counter>: int ; counter number
+            :param <counter>: int ; counter variable
             :param <error>: str ; error message
             """
-            if back_off_counter > 50: 
+            if counter > 50: 
                 log.error(error)
                 raise
-            print(f"The program will back off for {2**back_off_counter} seconds.")
+            print(f"The program will back off for {2**counter} seconds.")
             counter += 1
             time.sleep(2 ** counter)
             return counter
@@ -160,37 +163,39 @@ class img_metadata:
         iterations = self.instances
         back_off_counter = 0
         
-        # Checks for prior extracts
+        # Checks for prior extractions
         try: json_path = self.check_prior_extract()
         except (IOError, WindowsError) as error:
             log.error(error_message("IO or Windows", "crawl_metadata"))
             raise
         
+        # Saves existing data in the most recent JSON file
         with open(json_path, "r") as file: requested_id = json.load(file)
         json_local = requested_id
+        # Records the most recent retrieved ID (+&) based on the assumption that the JSON is sorted in descending order
         if requested_id == []: requested_id = 1
         else: requested_id = requested_id[0]["id"] + 1
 
         while True:
             try:
-                #time delay to respect the API's license
-                time.sleep(.200)
-                #checks iteration number
+                # Checks if iterations is a variable of type int
+                # If it is, subtracts 1 (iterations is a counter)
                 if type(iterations)==int:
                     if iterations > 0: iterations -= 1
                     else: 
                         print(f"{self.instances} images retrieved as requested.")
                         break
-                #checks existing data folder and target metadata file
+                # Checks if ./data folder and target metadata file exist
                 json_path = self.check_prior_extract(False)
-                #requests new derpibooru page
+                # Requests a new derpibooru page
                 print(f"You are requesting the derpibooru page starting with the id° {requested_id}.")
-                path_derpibooru = derpibooru_url + str(requested_id)
-                json_derpibooru = requests.get(path_derpibooru).json()["images"]
-                #raise of exception if derpibooru was fully scraped
+                json_derpibooru = requests.get(derpibooru_url + str(requested_id)).json()["images"]
+                # Raises exception in case derpibooru was fully scraped, i.e. request returned an empty list
                 if json_derpibooru == []: raise DatabaseFullyCrawled
-                #update content of metadata file
-                requested_id, json_local = self.json_collect(json_derpibooru, json_local, json_path) 
+                # Updates content of metadata file
+                requested_id, json_local = self.json_collect(json_derpibooru, json_local, json_path)
+                #time delay to respect the API's license
+                time.sleep(.200)
             except DatabaseFullyCrawled:
                 print("The crawler scraped the derpibooru metadata. The program will now close.")
             except (IOError, WindowsError):
@@ -219,16 +224,16 @@ class img_metadata:
         # Sorts metadata by ID
         for image_data in json_derpibooru:
             temp = image_data.copy()
-            # Deletes unwanted keys
+            # Deletes unwanted keys from each JSON entry
             for item in image_data: 
                 if item not in stored_keys: del temp[item]
             last_id = max(image_data["id"], last_id)
             json_local.append(temp)
             json_local.sort(key=operator.itemgetter("id"), reverse = True)
-        # Saves the json
+        # Saves the JSON
         with open(json_path,'w') as file: json.dump(json_local, file)
         
-        # Tests if json splitting is necessary
+        # Tests if the JSON is too large and needs to be split
         try: split = self.json_split_size(json_path)
         except (IOError, WindowsError) as error:
             log.error(error_message("IO or Windows", "json_collect"))
@@ -240,9 +245,10 @@ class img_metadata:
         """
         Retrieves the IDs of the locally stored metadata that fit specific tag parameters
         ---
-        :param <tags>: <list> ; list of strings (i.e. picture tags)
-        :param <at_least_one>: <boolean> ; toggles 'at least one tag' option instead of 'all tags'
+        :param <tags>: <list> ; list picture tags used for sorting and extracting metadata
+        :param <at_least_one>: <boolean> ; toggles whether or not data to be retrieved must satisfy at least one tag or all of them
         """
+        
         def any_or_all(boolean):
             """
             Returns the function any() if argument <boolean> TRUE, else returns function all()
@@ -252,8 +258,7 @@ class img_metadata:
             if boolean == True: return any
             else: return all
         
-        metadata_files_list = filter(lambda file: file.startswith("derpibooru_metadata"), 
-                                     os.listdir(os.getcwd() + "\\data"))
+        metadata_files_list = filter(lambda file: file.startswith("derpibooru_metadata"), os.listdir(os.getcwd() + "\\data"))
         metadata_files_list = list(metadata_files_list)
         id_list = []
         url_list = []
@@ -262,11 +267,15 @@ class img_metadata:
             try:
                 with open(os.getcwd() + "\\data\\" + fname,"r") as file: json_local = json.load(file)
                 if json_local == []: break
+                # Creates a list of the tags to keep based on each starting with a key character +
                 tags_keep = list(filter(lambda item: item.startswith("+"), tags))
                 tags_keep = list(map(lambda item: item[1:], tags_keep))
+                # Creates a list of the tags to keep based on each starting with a key character -
                 tags_remove = list(filter(lambda item: item.startswith("-"), tags))
                 tags_remove = list(map(lambda item: item[1:], tags_remove))
+                # Checks the requested filter
                 fltr = any_or_all(at_least_one)
+                # Filters out unwanted entries
                 filter_keep = lambda item: fltr(tag in item["tags"].split(", ") for tag in tags_keep)
                 filter_remove = lambda item: not any(tag in item["tags"].split(", ") for tag in tags_remove)
                 json_kept = list(filter(filter_keep, json_local))
@@ -284,8 +293,7 @@ class img_metadata:
     def repair_tags(self):
         """
         Checks if all retrieved IDs have an available list of tags.
-        ---
-        :param <self>: <class> ; class object reference
+        If not, repairs them by requested the derpibooru's API.
         """
         metadata_files_list = filter(lambda file: file.startswith("derpibooru_metadata"), os.listdir(os.getcwd() + "\\data"))
         metadata_files_list = list(metadata_files_list)
@@ -307,24 +315,24 @@ class img_metadata:
                 except AbsentTagList:
                     print(f"The url request for the picture {json_id} returned an empty list of tags.")
                 except (IOError, WindowsError) as error:
-                    log.error(io_windows_error)
+                    log.error(error_message("IO or Windows", "repair_tags"))
                     raise
                 except (requests.exceptions.TooManyRedirects, requests.exceptions.RequestException) as error:
-                    log.error(f"REQUEST ERROR: {error}.")
+                    log.error(error_message("Request [generic error]", "repair_tags"))
                     raise
                 except requests.exceptions.HTTPError as error:
-                    log.error(f"REQUEST [HTTP] ERROR: {error}.")
+                    log.error(error_message("Request [hhtp]", "repair_tags"))
                     raise
                 except requests.exceptions.ConnectionError as error:
-                    log.error(f"REQUEST [CONNECTION] ERROR: {error}.")
+                    log.error(error_message("Request [Connection]", "repair_tags"))
                     raise
                 except requests.exceptions.Timeout as error:
-                    log.error(f"REQUEST [TIMEOUT] ERROR: {error}.")
+                    log.error(error_message("Request [timeout]", "repair_tags"))
                     raise
             try:
                 with open(os.getcwd() + "\\data\\" + fname,'w') as file: json.dump(json_local, file)
             except (IOError, WindowsError) as error:
-                log.error(io_windows_error)
+                log.error(error_message("IO or Windows", "repair_tags"))
                 raise
 
 class Error(Exception):
